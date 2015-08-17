e1cd9dd58a6196fbc98eee8b4995b9d4536c866b
Debsir <simon22543@hotmail.com>
2015-08-17 09:39:24 +0800

    Update the send_mail.py

--- python/network_programming/send_mail.py
+++ python/network_programming/send_mail.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python
 
-import sys, smtplib, socket
+import sys, smtplib, socket, mimetypes
 from getpass import getpass
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email import utils, encoders
+
+def genpart(data, contenttype):
+    maintype, subtype = contenttype.split('/')
+    if maintype == 'text':
+        retval = MIMEText(data, _subtype=subtype)
+    else:
+        retval = MIMEBase(maintype, subtype)
+        retval.set_payload(data)
+        encoders.encode_base64(retval)
+    return retval
+
+def attachment(filename):
+    fd = open(filename, 'rb')
+    mimetype, mimeencoding = mimetypes.guess_type(filename)
+    if mimeencoding or (mimetype is None):
+        mimetype = 'application/octet-stream'
+    retval = genpart(fd.read(), mimetype)
+    retval.add_header('Content-Disposition', 'attachment',
+                      filename = filename)
+    fd.close()
+    return retval
 
 server = raw_input('Please enter the smtp server:\n')
 sys.stdout.write("Enter username:")
@@ -11,11 +36,23 @@ fromaddr = username
 toaddrs = raw_input('Please enter your recipients seperated by ",":\n') 
 subject = raw_input("You'd better give a mail subject:\n") or "No subject"
 message = raw_input("Now, it's time to write the mail.\n")
+attach_file = raw_input("Type the attach file name if needed:\n") or None
 
-message = """To: %s
-From: %s
-Subject: %s
-""" % (toaddrs, fromaddr, subject) + '\n' + message
+msg = MIMEMultipart()
+msg['To'] = toaddrs
+msg['From'] = fromaddr
+msg['Subject'] = subject
+msg['Date'] = utils.formatdate(localtime=1)
+msg['Message-ID'] = utils.make_msgid()
+
+body = MIMEMultipart('alternative')
+body.attach(genpart(message, 'text/plain'))
+msg.attach(body)
+
+if attach_file:
+    msg.attach(attachment(attach_file))
+
+mail = msg.as_string()
 
 try:
     s = smtplib.SMTP(server)
@@ -25,7 +62,7 @@ try:
         print "Authentication failed:", e
         sys.exit(1)
     toaddrs = toaddrs.split(',')
-    s.sendmail(fromaddr, toaddrs, message)
+    s.sendmail(fromaddr, toaddrs, mail)
 except (socket.gaierror, socket.error, socket.herror, 
         smtplib.SMTPException), e:
     print " *** Your message may not have been sent!"
@@ -33,3 +70,4 @@ except (socket.gaierror, socket.error, socket.herror,
     sys.exit(1)
 else:
     print "Message sent to %d recipient(s)" % len(toaddrs)
+
